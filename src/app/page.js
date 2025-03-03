9c9e5bd468d4554a12b96991b3db0e69712995cd
kpopera <kpopera123@gmail.com>
2025-03-04 02:05:24 +0800

    fixed mobile view technical and about me bug

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import TechnicalSkills from "@/components/TechnicalSkills";
 import ContactText from "@/components/ContactText";
 import FloatingIcons from "../components/FloatingIcons";
 import Cover from "@/components/Cover";
-import Head from "next/head";
 
 export default function Home() {
   return (
@@ -17,7 +16,7 @@ export default function Home() {
       </div>
 
       {/* Start ME */}
-      <div
+      {/* <div
         id="bio"
         className="bg-secondaryColor p-3  h-screen mb-4 sm:mb-36 md:mb-21 lg:mb-6 xl:mb-0"
       >
@@ -38,22 +37,48 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Start ProjectCard */}
-      {/* <div id="project" className="pt-14 md:mt-8">
-        <h1 className="text-3xl border-l-4 border-mainColor p-2 font-roboto font-semibold ml-3 opacity-40">
-          PROJECT
-        </h1>
-        <div className="p-4 md:p-8">
-          <ProjectCard />
-        </div>
-      </div> */}
-
-      {/* Technical Skills */}
       <div id="TechnicalSkills" className="  p-3  h-screen">
         <h1 className="text-3xl border-l-4 border-mainColor  font-roboto font-semibold p-2 mb-2 opacity-40">
           TECHNICAL SKILL
         </h1>
         <div className="p-3 mb-44">
+          <TechnicalSkills />
+        </div>
+      </div> */}
+
+      <div
+        id="bio"
+        className="bg-secondaryColor p-3 min-h-screen mb-10 sm:mb-24 md:mb-32 lg:mb-20 xl:mb-10 w-full"
+      >
+        <h1 className="text-2xl sm:text-3xl border-l-4 border-mainColor p-2 font-roboto font-semibold opacity-40 mt-6 sm:mt-10">
+          ME
+        </h1>
+        <div className="grid sm:grid-cols-1 md:grid-cols-[25%_75%] gap-4 w-full h-auto p-4 sm:p-6 bg-secondaryColor mb-20">
+          {/* Profile Photo Section */}
+          <div className="bg-secondaryColor w-full p-3 grid justify-center items-center">
+            <img
+              src="/assets/photo_white.png"
+              alt="Profile Photo"
+              className="rounded-full object-cover w-24 sm:w-40 md:w-60 lg:w-96 max-w-full"
+            />
+          </div>
+
+          {/* Details Section */}
+          <div className="bg-secondaryColor text-lg sm:text-xl md:text-xl border-l-4 md:border-l-8 border-mainColor mt-4 md:mt-0 md:ml-6 p-4 sm:p-5 md:p-6 w-full">
+            <Details />
+          </div>
+        </div>
+      </div>
+
+      {/* Technical Skills Section */}
+      <div
+        id="TechnicalSkills"
+        className="p-3 sm:p-4 md:p-6 min-h-screen w-full"
+      >
+        <h1 className="text-2xl sm:text-3xl border-l-4 border-mainColor font-roboto font-semibold p-2 mb-4 opacity-40">
+          TECHNICAL SKILL
+        </h1>
+        <div className="p-2 sm:p-3 mb-20 md:mb-44 w-full">
           <TechnicalSkills />
         </div>
       </div>
@@ -70,10 +95,10 @@ export default function Home() {
           <div className="pt-10 p-3 ">
             <Contact />
           </div>
-          <FloatingIcons />
         </div>
       </div>
-      {/* END */}
+      {/* START FLOATING ICON */}
+      <FloatingIcons />
     </div>
   );
 }
