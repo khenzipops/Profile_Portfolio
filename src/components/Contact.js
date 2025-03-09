dd590501c59b538211568a29653967563328b981
kpopera <kpopera123@gmail.com>
2025-03-10 03:14:54 +0800

    minor update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,11 +22,14 @@ export default function Contact() {
     setFeedback(null); // Reset feedback message
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/contact`, // API URL
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
