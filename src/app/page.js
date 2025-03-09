e523e912c3c40bd00e76ae9fe9c201735eb9b60b
kpopera <kpopera123@gmail.com>
2025-03-10 03:59:07 +0800

    minor update

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import ContactText from "@/components/ContactText";
 import FloatingIcons from "../components/FloatingIcons";
 import Cover from "@/components/Cover";
 import useInView from "../hooks/useinView";
-import ContactInfo from "@/components/ContactInfo";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   const coverRef = useRef(null);
@@ -89,7 +89,7 @@ export default function Home() {
             <ContactText />
           </div>
           <div className="pt-10 p-3">
-            <ContactInfo />
+            <Contact />
           </div>
         </div>
       </div>
